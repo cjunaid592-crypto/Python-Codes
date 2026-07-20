a99816d05e41d73e83e69dda66cb089e0557169f
cjunaid592-crypto <cjunaid592@gmail.com>
2026-07-20 12:00:00 +0500

    I'm Coder beginner
    Python Programs

---
+++ newfile.py
@@ -0,0 +1,23 @@
+# calculate Math sum 
+
+if : Value of = y > 3
+                         x > 6
+                        "Find out"
+x = int, intput("Enter the first number")
+y = int, input("Enter the second number")
+print("Sum" ,  y+x )
+print("difference", y-x)
+print("product", y*x) [
+
+]
+
+elif: "First value ans" / 9
+        "Second value ans" / -3
+          
+finally : "Third value ans" / 18 ;
+
+)
+    
+("Program, finished ! ")
+
+
